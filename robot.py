51b8cebb98441cb5407064fa0c37a6cbc9c62ef5
rtuttle <rtuttle@irobot.com>
2021-08-17 09:04:13 -0400

    small changes

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ class Robot():
     class Wheel():
         def __init__(self, pin):
             self.pin = 0
-            self.vel = 0
+            self.vel = 0.0
         def set_vel(self, new_vel):
             self.vel = new_vel
     def __init__(self):
@@ -31,15 +31,15 @@ class Robot():
         """ process controller state into something that the robot can understand
             right now that is just to convert the 
         """
-        left_vel = controller_state.left_stick.x / -32768
-        right_vel = controller_state.right_stick.x / -32768
+        left_vel = controller_state.left_stick.x / -32768.0
+        right_vel = controller_state.right_stick.x / -32768.0
         self.set_vels(left_vel, right_vel)
         if controller_state.square == "down":
             """ because there can be some issues with latching the last joystick values
                 we sometimes need a specific button to hit the brakes
             """
             controller_state.zero_sticks()
-            self.set_vels(0, 0)
+            self.set_vels(0.0, 0.0)
     def print_vels(self):
         """ print out the left and right wheel vels
             assuming for the moment the wheel vels are the same per side

b2428ea20e38ce32a50f6c07d04a2333437d2bb2
rtuttle <rtuttle@irobot.com>
2021-08-27 10:37:35 -0400

    adding logging utilities

--- ez_cart_runner.sh
+++ ez_cart_runner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-EEB_HOME=/home/pi/electric_eel_barrow
 EEB_USER=pi
+EEB_HOME=/home/pi/electric_eel_barrow
 
 EXIT_CODE=1
-# while [ $EXIT_CODE -ne 0 ]; do
-while [ 1 ]; do
-    sudo -H -u $EEB_USER python3 $EEB_HOME/ez_cart_main.py
+while [ $EXIT_CODE -ne 0 ]; do
+# while [ 1 ]; do
+    sudo -H -u $EEB_USER python3 $EEB_HOME/ez_cart_main.py >> $EEB_HOME/logs/main_log.txt
     EXIT_CODE=$?
 done

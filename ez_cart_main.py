2973967f1a1ea6e9a2276fea235cb9d404cb1633
Riley Tuttle <rileytuttle@gmail.com>
2021-08-27 10:37:35 -0400

    adding logging utilities

--- ez_cart_main.py
+++ ez_cart_main.py
@@ -4,6 +4,8 @@ from controller_state import ControllerState
 from controller_parser import MyController
 from robot import Robot
 from time import sleep
+import os
+from logging_utils import log
 
 import threading
 
@@ -38,7 +40,7 @@ def connect_controller(params):
     while not params.controller.is_connected:
         params.controller_state = ControllerState()
         params.controller = MyController(controller_state=params.controller_state, interface="/dev/input/js0", connecting_using_ds4drv=False)
-        print(f'trying to reconnect to /dev/input/js0')
+        log(f'trying to reconnect to /dev/input/js0')
         sleep(1)
 
 def setup():
@@ -49,13 +51,18 @@ def runner():
     try:
         params = setup()
         application(params)
+    except KeyboardInterrupt as ki:
+        log(f'{ki}')
+        os._exit(os.EX_OK)
     except Exception as e:
         # pdb.set_trace()
         # params.thread.join()
         # sleep(2) # sleep for 2 seconds before trying again
         # runner()
-        pass
-
+        log(f'runner caught exception {e}')
+        # raise Exception()
+        os._exit(1)
+        
 def application(params):
     while True:
         if not params.thread.is_alive():

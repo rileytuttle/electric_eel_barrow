18e63bdd6fc9b26f03f594bca8e090e183182900
Riley Tuttle <rileytuttle@gmail.com>
2022-04-08 08:44:40 -0400

    wip

--- start_ez_cart_ros.sh
+++ start_ez_cart_ros.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -e /dev/input/js0 ]]
+then
+    echo "controller connected"
+else
+    echo "controller not connected exiting docker to start up again"
+    kill 1
+fi
+
 source /electric_eel_barrow/ez_cart_ros/install/setup.bash
 
 ros2 launch /electric_eel_barrow/ez_cart_ros/launch/ez_cart_launch.py

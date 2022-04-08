2dd461193501a1473e87ca47a66d27f2b10f717f
Riley Tuttle <rileytuttle@gmail.com>
2022-04-07 21:35:35 -0400

    wip. ... need to make a start up script that runs the ds4 connector in a separate process and then runs the docker in a separate process. the js0 input will be passed through

--- start_ez_cart_ros.sh
+++ start_ez_cart_ros.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-/electric_eel_barrow/setup_bluetooth_connection.sh
-/electric_eel_barrow/ds4_connection_manager.sh
-
-sleep 5 #needs this otherwise the joy node doesn't talk to the bluetooth controller
-
 source /electric_eel_barrow/ez_cart_ros/install/setup.bash
 
 ros2 launch /electric_eel_barrow/ez_cart_ros/launch/ez_cart_launch.py
--- start_ros_docker.sh
+++ start_ros_docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo killall -9 bluetoothd
-sudo docker run --rm -it --mount type=bind,source="$(pwd)",target=/electric_eel_barrow --privileged --net=host --name="ez_cart_docker" ros_docker:latest
+sudo docker run --rm -it --mount type=bind,source="$(pwd)",target=/electric_eel_barrow --privileged --device=/dev/input/js0 --net=host --name="ez_cart_docker" ros_docker:latest

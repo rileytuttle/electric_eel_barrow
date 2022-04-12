e6df4240ce92b29f1433296629576489a97d77af
Riley Tuttle <rileytuttle@gmail.com>
2022-04-12 13:22:20 -0400

    working on camera stuff

--- create_ez_cart_container_for_startup.sh
+++ create_ez_cart_container_for_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker create --privileged --network=host --init --name="ez_cart_docker" --device/dev/input/js0 --device=/dev/video0 --mount type=bind,source=/home/pi/electric_eel_barrow,target=/electric_eel_barrow ezcart_docker_image:latest /electric_eel_barrow/start_ez_cart_ros.sh
+sudo docker create --privileged --network=host --init --name="ez_cart_docker" --device=/dev/input/js0 --device=/dev/video0 --mount type=bind,source=/home/pi/electric_eel_barrow,target=/electric_eel_barrow ezcart_docker_image:latest /electric_eel_barrow/start_ez_cart_ros.sh
